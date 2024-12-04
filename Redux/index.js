5f2c27697eeefb33375cfd5c68294410b159a5aa
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-12-04 18:55:20 +0530

    core redux done

--- Redux/index.js
+++ Redux/index.js
@@ -4,10 +4,15 @@ import { thunk } from "redux-thunk"; // Default export for redux-thunk
 import axios from "axios";
 
 // Constants
-const INC = "increment";
-const DEC = "decrement";
-const INC_BY_AMT = "incrementByAmount";
-const INIT = "init";
+const INC = "amount/increment";
+const DEC = "amount/decrement";
+const INC_BY_AMT = "amount/incrementByAmount";
+// const INIT = "amount/init";
+const bonusINC = "bonus/increment";
+const BNS_INC_BY_AMT = "bonus/incrementByAmount";
+const GET_USER_DATA_SUCCESS = 'amount/success';
+const GET_USER_DATA_PENDING = 'amount/pending';
+const GET_USER_DATA_REJECT = 'amount/error';
 
 // Store
 const store = createStore(
@@ -20,8 +25,12 @@ const store = createStore(
 // Reducer
 function amountReducer(state = { amount: 1 }, action) {
   switch (action.type) {
-    case INIT:
-      return { amount: action.payload };
+    case GET_USER_DATA_SUCCESS:
+      return { amount: action.payload , pending:false};
+    case GET_USER_DATA_PENDING:
+      return {...state,pending:true} 
+    case GET_USER_DATA_REJECT:
+      return {...state,error:action.error, pending:false}   
     case INC:
       return { amount: state.amount + 1 };
     case DEC:
@@ -34,9 +43,11 @@ function amountReducer(state = { amount: 1 }, action) {
 }
 function bonusReducer(state = { points: 0 }, action) {
   switch (action.type) {
-    case INC_BY_AMT:
+    case BNS_INC_BY_AMT:
         if(action.payload >= 100)
-          return { points: state.points + 1 };
+          return { points: state.points + 10 };
+    case bonusINC:
+        return {points:state.points+1}    
     default:
       return state;
   }
@@ -59,19 +70,41 @@ function decrement() {
 function incrementByAmount(value) {
   return { type: INC_BY_AMT, payload: value };
 }
-
+function bonusIncreByAmount(value){
+  return {type:BNS_INC_BY_AMT,payload:value};
+}
+function bonusIncrement(){
+  return {type:bonusINC}
+}
+function accountDataSuccess(value){
+  return {type:GET_USER_DATA_SUCCESS,payload:value}
+}
+function accountDataReject(error){
+  return {type:GET_USER_DATA_REJECT,error:error}
+}
+function accountDataPending(){
+  return {type:GET_USER_DATA_PENDING}
+}
 // Async Action Creator (Thunk)
-function getUser(id) {
+function getUserAccount(id) {
   return async (dispatch, getState) => {
     try {
+      // dispatch(accountDataPending());
       const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
-      dispatch(initUser(data.amount));
+      dispatch(accountDataSuccess(data.amount));
     } catch (error) {
-      console.error("Failed to fetch user data:", error);
+      dispatch(accountDataReject(error.message))
     }
   };     
 }
 
 // Dispatching Async Action
 // store.dispatch(getUser(2));
-store.dispatch(incrementByAmount(300))
+// store.dispatch(incrementByAmount(300))
+setInterval(() => {
+  // store.dispatch(incrementByAmount(300))
+    //  store.dispatch(bonusIncrement());
+    //  store.dispatch(bonusIncreByAmount(200));
+   store.dispatch(getUserAccount(1));
+
+}, 5000);
