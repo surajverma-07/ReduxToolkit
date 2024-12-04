25faa196126a9e6ee43ca9ff306ba93f85f4b8d4
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-12-04 20:10:26 +0530

    redux in react app

---
+++ ReactRedux/src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+import { createStore,combineReducers,applyMiddleware } from 'redux'
+import {thunk} from 'redux-thunk'
+import logger from 'redux-logger'
+import { accountReducer } from './reducers/Account.jsx'
+import { bonusReducer } from './reducers/Bonus.jsx'
+
+const store = createStore(
+  combineReducers({
+       amount:accountReducer,
+       bonus:bonusReducer
+  }),
+  applyMiddleware(logger, thunk));
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+
+    <App />
+    </Provider>
+  </StrictMode>
+)
